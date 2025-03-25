69fc50b75eccebe28d83f764ed3263ae26c8ee59
DeclanFitzpatrick <declan.fitzpatrick@ucalgary.ca>
2025-03-25 09:47:30 -0600

    got import lab04 to work

--- examples/driver.py
+++ examples/driver.py
@@ -1,14 +1,13 @@
 import numpy as np
 
-from src.lab_04.regression import (multi_regress)
+from lab_04.regression import (multi_regress)
 
 def main():
-    data = np.loadtxt("M_data1.txt")
+    data = np.loadtxt("data/M_data1.txt")
+    
     y = data[:,0] # enter dependent variable data (or load from a file)
     Z = data[:,1] # enter independent variable data (or load from a file)
     a, e, rsq = multi_regress(y, Z)
-
-
 
 if __name__ == "__main__":
     main()
