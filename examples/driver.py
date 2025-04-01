efb31b6a1fef63eab10adc2eefbab78d2fc83466
DeclanFitzpatrick <declan.fitzpatrick@ucalgary.ca>
2025-04-01 10:28:47 -0600

    Made first grpah for 24gr period with line regress

--- examples/driver.py
+++ examples/driver.py
@@ -9,30 +9,14 @@ def main():
     t = data[:,0]  # dependent data
     m_data = data[:,1]  # independent data
 
-    k_24 = np.argwhere(t < 24)[-1]  # find indices when t is less than 24 and select last index
-    print(f'time (24): {k_24}')
-    print(f'time at k_24: {t[k_24]}')
+    k_24 = np.argwhere(t < 24)[-1].item()
 
-
-    k_24 = int(k_24)
-    t = t[0:k_24]  # slice t to only include values less than 24
-    print(t[0:k_24])
-
-
-    mk_24 = np.argwhere(m_data < 24)[-1]  # find indices when t is less than 24 and select last index
-    print(f'm data (24): {mk_24}')
-    print(f'm data at mk_24: {m_data[mk_24]}')
-
-    mk_24 = int(mk_24)  # convert to int for indexing
-    m_data = m_data[0:k_24]  # slice m_data to only include values less than 24
-    print(m_data[0:k_24])
-
-    m = np.linspace(-0.5, 1.5, 9)
+    m = np.linspace(-0.5, 1, 9)
     n = np.zeros_like(m)  # array of zeros same size as m
 
     for i, mm in enumerate(m):  # loop through m and count values in m_data that are greater than or equal to mm
-        n[i] = np.count_nonzero(m_data >= mm)  # count occurrences
-
+        n[i] = np.count_nonzero(m_data[:k_24] >= mm)  # count occurrences
+    print(n)
     y = np.log10(n)
     Z = np.vstack((np.ones_like(m), m)).T  # design matrix
 
@@ -42,16 +26,14 @@ def main():
     print(f' e: {e}')
     print(f' rsq: {rsq}')
 
+    Log1 = Z @ a  # compute predicted values
+
+
     plt.plot(m, y, 'o')
+    plt.plot(m, Log1, 'o')
     plt.xlabel('m')
     plt.ylabel('log10(n)')
     plt.title('log10(n) vs m')
-    plt.show()
-
-    plt.plot(t, m_data, 'o')
-    plt.xlabel('time')
-    plt.ylabel('earthquake magnitude')
-    plt.title('Earthquake Magnitude as a function of Time')
     plt.show()
 
 
