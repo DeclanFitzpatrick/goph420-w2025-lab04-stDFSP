46c0edbe7ad044281b9730b9dd7a1c89001d6608
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-04-01 13:35:07 -0600

    fixed raw data graph with lines shwoing major breaks

--- examples/raw_driver.py
+++ examples/raw_driver.py
@@ -4,33 +4,36 @@ from lab_04.regression import (multi_regress)
 
 
 def main():
-    data = np.loadtxt("data/M_data1.txt")
+    data = np.loadtxt("../data/M_data1.txt")
 
     t = data[:,0]  # dependent data
     m_data = data[:,1]  # independent data
 
-    k_24 = np.argwhere(t < 24)[-1]  # find indices when t is less than 24 and select last index
-    print(f'time (24): {k_24}')
-    print(f'time at k_24: {t[k_24]}')
+    k_120 = np.argwhere(t < 120)[-1]  # find indices when t is less than 120 and select last index
+    print(f'time (120): {k_120}')
+    print(f'time at k_120: {t[k_120]}')
 
+    k_120 = int(k_120)
+    t = t[0:k_120]  # slice t to only include values less than 120
+    print(t[0:k_120])
 
-    k_24 = int(k_24)
-    t = t[0:k_24]  # slice t to only include values less than 24
-    print(t[0:k_24])
+    mk_120 = np.argwhere(m_data < 120)[-1]  # find indices when t is less than 120 and select last index
+    print(f'm data (120): {mk_120}')
+    print(f'm data at mk_120: {m_data[mk_120]}')
 
+    m_data = m_data[0:k_120]  # slice m_data to only include values less than 120
+    print(m_data[0:k_120])
 
-    mk_24 = np.argwhere(m_data < 24)[-1]  # find indices when t is less than 24 and select last index
-    print(f'm data (24): {mk_24}')
-    print(f'm data at mk_24: {m_data[mk_24]}')
-
-    mk_24 = int(mk_24)  # convert to int for indexing
-    m_data = m_data[0:k_24]  # slice m_data to only include values less than 24
-    print(m_data[0:k_24])
-
-    plt.plot(t, m_data, 'o')
+    plt.figure(figsize=(12,6))
+    plt.plot(t, m_data, 'go', markersize=2)
+    x_list = [34,46,72,96]
+    for x in x_list:
+        plt.axvline(x=x, color='r',linestyle='--')
+        plt.text(x + 0.5, max(m_data), f"{x} hours", color='r', fontsize=10)
     plt.xlabel('time')
     plt.ylabel('earthquake magnitude')
     plt.title('Earthquake Magnitude as a function of Time')
+    plt.savefig('../figures/magnitude_vs_time.png')
     plt.show()
 
 
